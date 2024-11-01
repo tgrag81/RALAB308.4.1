327b438add9f8a7b57a1a5cfb0c215994b1d50d1
Terence A Gragston <terence.gragston@gmail.com>
2024-11-01 12:16:59 -0400

    Add files via upload

--- loops.js
+++ loops.js
@@ -7,6 +7,36 @@
 //If number is NOT divisible by either, simply log the number
 //Remember to interate and committ
 
+function fizzBuzz(n) {
+
+    let result = [];
+
+    for (let i = 1; i <= n; ++i) {
+    
+        if (i % 3 === 0 && i % 5 === 0) {
+        
+            result.push("FizzBuzz");
+        } 
+        
+        else if (i % 3 === 0) {
+        
+            result.push("Fizz");
+        } 
+        
+        else if (i % 5 === 0) {
+        
+            result.push("Buzz");
+        } 
+        else {
+            result.push(i.toString());
+        }
+    }
+    return result;
+}
+
+let n = 10;
+let result = fizzBuzz(n);
+console.log(result.join(' '));
 
 
 
@@ -30,3 +60,5 @@
 
 //Use the example string provided above to test your program. Once you are confident it is working correctly, try the following string to see if your program works properly with other data.
 //Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232
+
+
